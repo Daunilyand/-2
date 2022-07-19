307c42ca77b3cb76a699a5ea20591427efce0304
Daunilyand <108757938+Daunilyand@users.noreply.github.com>
2022-07-20 02:12:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+x = 5
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+print(x ** 0.5)
+print(pow(x, y))
+print(3.2 * 0.8 - 3.9 - 3 ** 4)
+
+number = 4.678
+print(round(number, 1))
